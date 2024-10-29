cbac1e999c737ad06a5b04597912af56d16a415e
zac273017 <zac273017@gmail.com>
2024-10-29 11:56:17 +0000

    update: update app.py

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ s3 = boto3.client(
     's3',
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-    region_name='your-region'
+    region_name=os.getenv('REACT_APP_AWS_REGION')
 )
 
-BUCKET_NAME = "your-bucket-name"
+BUCKET_NAME = os.getenv('REACT_APP_AWS_BUCKET_NAME')
 
 # Route for the main page
 @app.route('/')
